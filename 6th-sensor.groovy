d710103d65c4ec638aed13b1f7e4eb68ffb15c23
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-05-05 10:31:58 -0700

    Improve 6th Sensor

--- 6th-sensor.groovy
+++ 6th-sensor.groovy
@@ -17,47 +17,121 @@
  */
 
 definition(
-    name: "6th Sensor",
-    namespace: "primalmotion",
-    author: "primalmotion",
-    description: "Turn on lights when dark, deal with night path, and more",
-    category: "Convenience",
-    iconUrl: "http://icons.iconarchive.com/icons/graphicloads/medical-health/96/eye-icon.png",
-    iconX2Url: "http://icons.iconarchive.com/icons/graphicloads/medical-health/96/eye-icon.png"
+    name:           "6th Sensor",
+    namespace:      "primalmotion",
+    author:         "primalmotion",
+    description:    "Turn on lights when dark, deal with night path, and more",
+    category:       "Convenience",
+    iconUrl:        "http://icons.iconarchive.com/icons/graphicloads/medical-health/96/eye-icon.png",
+    iconX2Url:      "http://icons.iconarchive.com/icons/graphicloads/medical-health/96/eye-icon.png"
 )
+
+
+
+/*
+    Preferences
+*/
 
 preferences
 {
-    section ("Sensors")
-    {
-        input "_lightsensordevice", "capability.illuminanceMeasurement", title: "Select the light sensor to control", required: true
-        input "_motionsensordevice", "capability.motionSensor", title: "Select the motion sensor to control", required: true
-    }
+    page(name: "preferencesRootPage")
+    page(name: "preferencesNightLights")
+    page(name: "preferencesNightPath")
+    page(name: "preferencesNightWatch")
+}
 
-    section ("Night Light")
-    {
-        input "_nightlightsenabled", "boolean", title: "Enable Night Light", required: true
-        input "_homemode", "mode", title: "When home is in mode", required: false
-        input "_nightlights", "capability.switch", title: "Turn these lights on", multiple: true, required: false
-        input "_luminancetrigger", "number", title: "When luminance is below: (default: 300lux)", required: false
-        input "_nightlighttime", "number", title: "Turn them off after no motion for (default: 600s)", required: false
-    }
 
-    section ("Night Path")
+def preferencesRootPage()
+{
+    dynamicPage(name: "preferencesRootPage", title: "preferencesRootPage", uninstall: true, install: true)
     {
-        input "_nightpathenabled", "boolean", title: "Enable Night Path", required: true
-        input "_nightmode", "mode", title: "When home is in mode", required: false
-        input "_pathligths", "capability.switch", title: "On motion, turn these lights on", multiple: true, required: false
-        input "_pathtime", "number", title: "Turn them off after (default: 60s)", required: false
-    }
+        section("6th Sensor")
+        {
+            paragraph "Welcome to 6th Sensor. This Smart App can group a few light and motion sensors, and make them work together to decide when and how lights should be turned on or off."
+        }
 
-    section ("Night Watch")
-    {
-        input "_nightwatchenabled", "boolean", title: "Enable Night Watch", required: true
-        input "_awaymode", "mode", title: "Send alert on any motion when home is in this mode", required: false, defaultValue: "Away"
+        section("Motion Sensors")
+        {
+            paragraph "Motion sensors used to make 6th Sensor trigger an illuminance check"
+            input "motionsensordevices", "capability.motionSensor", title: "Motion Sensors", required: true, multiple: true
+        }
+
+        section("Light Sensor")
+        {
+            paragraph "Light sensor used to determine the global illuminance"
+            input "lightsensordevice", "capability.illuminanceMeasurement", title: "Light Sensors", required: true
+            input "luminancetrigger", "number", title: "Consider dark when illuminance is below (LUX): ", required: false, defaultValue: 300
+        }
+
+        section ("6th Sensor Configuration")
+        {
+            href(name: "topreferencesNightLights", page: "preferencesNightLights", title: "Night Lights")
+            href(name: "topreferencesNightPath", page: "preferencesNightPath", title: "Night Path")
+            href(name: "topreferencesNightWatch", page: "preferencesNightWatch", title: "Night Watch")
+        }
     }
 }
 
+def preferencesNightLights()
+{
+    dynamicPage(name: "preferencesNightLights", title: "preferencesNightLights", submitOnChange: true)
+    {
+        section ("Night Light")
+        {
+            paragraph "Night Light will turn light on and off based on motion and global illuminance when you are at home."
+            input "nightlightsenabled", "boolean", title: "Enable Night Light", required: true
+        }
+
+         section ("Settings")
+         {
+            input "homemode", "mode", title: "When home is in mode", required: false, defaultValue: "Home"
+            input "nightlights", "capability.switch", title: "Turn these lights on", multiple: true, required: false
+            input "nightlighttime", "number", title: "Turn off after no motion detected for (min)", required: false, defaultValue: 10
+        }
+    }
+}
+
+def preferencesNightPath()
+{
+    dynamicPage(name: "preferencesNightPath", title: "preferencesNightPath", submitOnChange: true)
+    {
+        section ("Night Path")
+        {
+            paragraph "Night Path will turn on a few lights for a bit when motion is detected during night."
+            input "nightpathenabled", "boolean", title: "Enable Night Path", required: true
+        }
+
+        section ("Settings")
+        {
+            input "nightmode", "mode", title: "When home is in mode", required: false,  defaultValue: "Night"
+            input "pathligths", "capability.switch", title: "On motion, turn these lights on", multiple: true, required: false
+            input "pathtime", "number", title: "Turn them off after (seconds)", required: false, defaultValue: 60
+        }
+    }
+}
+
+def preferencesNightWatch()
+{
+    dynamicPage(name: "preferencesNightWatch", title: "preferencesNightWatch", submitOnChange: true)
+    {
+        section ("Night Watch")
+        {
+            paragraph "Night Watch will send you a notification when motion is detected while nobody is home."
+            input "nightwatchenabled", "boolean", title: "Enable Night Watch", required: true
+        }
+
+        section ("Settings")
+        {
+            input "awaymode", "mode", title: "Send alert on any motion when home is in this mode", required: false, defaultValue: "Away"
+        }
+    }
+}
+
+
+
+/*
+    SmartThings Installation Handlers
+*/
 def installed()
 {
     initialize()
@@ -71,108 +145,150 @@ def updated()
 
 def initialize()
 {
-    final _luminancetrigger  = _luminancetrigger  ?: 300
-    final _nightlighttime    = _nightlighttime    ?: 10 * 60
-    final _pathtime          = _pathtime          ?: 60
-    final _homemode          = _homemode          ?: "Home"
-    final _nightmode         = _nightmode         ?: "Night"
-    final _awaymode          = _awaymode          ?: "Away"
+    log.debug("[initialize]: Settings: ${settings}")
 
-    log.debug("_luminancetrigger :${_luminancetrigger}")
-    log.debug("_nightlighttime   :${_nightlighttime}")
-    log.debug("_pathtime         :${_pathtime}")
-    log.debug("_homemode         :${_homemode}")
-    log.debug("_nightmode        :${_nightmode}")
-    log.debug("_awaymode         :${_awaymode}")
+    subscribe(lightsensordevice, "illuminance", on_event)
 
-    subscribe(_lightsensordevice, "illuminance", on_event)
-    subscribe(_motionsensordevice, "motion.active", on_event)
+    for (d in settings.motionsensordevices)
+        subscribe(d, "motion.active", on_event)
 }
 
 
+
+/*
+    Events Management
+*/
+
 def on_event(evt)
 {
-    log.debug("-------------------------")
+    log.debug("[on_event]: ------------- START ------------")
 
-    if (_nightlightsenabled && location.mode == _homemode)
+    if (settings.nightlightsenabled && location.mode == settings.homemode)
         on_home_event(evt)
 
-    else if (_nightpathenabled && location.mode == _nightmode)
+    else if (settings.nightpathenabled && location.mode == settings.nightmode)
         on_night_event(evt)
 
-    else if (_nightwatchenabled && location.mode == _awaymode)
+    else if (settings.nightwatchenabled && location.mode == settings.awaymode)
         on_away_event(evt)
 
-    log.debug("-------------------------")
+    log.debug("[on_event]: -------------- END -------------")
 }
 
 def on_home_event(evt)
 {
-    log.debug("home event: name: ${evt.name}, value: ${evt.value}")
+    log.debug("[on_home_event]: name: ${evt.name}, value: ${evt.value}, from ${evt.displayName}")
 
-    def is_dark = _lightsensordevice.currentIlluminance < _luminancetrigger
+    def dark   = is_light_sensor_in_dark()
+    def active = are_motion_sensors_active()
 
-    if (evt.name == "motion" && evt.value == "active")
-        is_dark ? turn_nightlights_on() : turn_nightlights_off()
+    log.debug("[on_home_event]: dark: ${dark}, active: ${active}")
 
-    else if (evt.name == "illuminance" && !is_dark)
-        turn_nightlights_off()
+    if (evt.name == "motion" && active)
+        dark ? turn_nightlight_lights_on() : turn_nightlight_lights_off()
+
+    else if (evt.name == "illuminance" && !dark)
+        turn_nightlight_lights_off()
 }
 
 def on_night_event(evt)
 {
-    log.debug("night event: name: ${evt.name}, value: ${evt.value}")
+    log.debug("[on_night_event]: night event: name: ${evt.name}, value: ${evt.value}, from ${evt.displayName}")
 
-    if (evt.name != "motion" || evt.value != "active")
+    if (evt.name != "motion" || !are_motion_sensors_active())
         return
 
-    turn_night_path_lights_on()
+    turn_nightpath_lights_on()
 
-    unschedule("turn_night_path_lights_off")
-    runIn(_pathtime, "turn_night_path_lights_off")
+    unschedule("turn_nightpath_lights_off")
+    runIn(settings.pathtime, "turn_nightpath_lights_off")
 }
 
 def on_away_event(evt)
 {
-    log.debug("away event: name: ${evt.name}, value: ${evt.value}")
+    log.debug("[on_away_event]: name: ${evt.name}, value: ${evt.value}, from ${evt.displayName}")
 
-    if (evt.name != "motion" || evt.value != "active" )
+    if (evt.name != "motion" || !are_motion_sensors_active())
         return
 
-    sendPush("Alert! Some movement has been detected but nobody's home!")
+    send_push_alert("Alert! Some movement has been detected but nobody's home!")
 }
 
-def turn_nightlights_on()
+
+
+/*
+    Night Ligths Utilities
+*/
+
+def turn_nightlight_lights_on()
 {
-    log.debug(" - turn lights on")
-    _set_lights_state(_nightlights, true)
+    log.debug("[turn_nightlight_lights_on]: turning lights on")
+    set_lights_state(settings.nightlights, true)
 
-    log.debug(" - canceling any secheduled light off.")
-    unschedule("turn_nightlights_off")
+    log.debug("[turn_nightlight_lights_on]: canceling any secheduled light off.")
+    unschedule("turn_nightlight_lights_off")
 
-    log.debug(" - schedulinng lights off in ${_nightlighttime} seconds")
-    runIn(_nightlighttime, "turn_nightlights_off")
+    log.debug("[turn_nightlight_lights_on]: scheduling may_turn_nightlight_lights_off in ${settings.nightlighttime} minutes")
+    runIn(settings.nightlighttime * 60, "may_turn_nightlight_lights_off")
 }
 
-def turn_nightlights_off()
+def may_turn_nightlight_lights_off()
 {
-    log.debug(" - turn lights off")
-    _set_lights_state(_nightlights, false)
+    if (are_motion_sensors_active())
+    {
+        log.debug("[may_turn_nightlight_lights_off]: one motion sensor is still active. rescheduling myself to run in ${settings.nightlighttime} min")
+        runIn(settings.nightlighttime * 60, "may_turn_nightlight_lights_off")
+    }
+    else
+    {
+        log.debug("[may_turn_nightlight_lights_off]: no motion sensor remaining active. turning lights off")
+        turn_nightlight_lights_off()
+    }
 }
 
-def turn_night_path_lights_on()
+def turn_nightlight_lights_off()
 {
-    log.debug(" - turn path lights on")
-    _set_lights_state(_pathligths, true)
+    log.debug("[turn_nightlight_lights_off]: turning lights off")
+    set_lights_state(settings.nightlights, false)
 }
 
-def turn_night_path_lights_off()
+
+
+/*
+    Night Path Utilities
+*/
+
+def turn_nightpath_lights_on()
 {
-    log.debug(" - turn path lights off")
-    _set_lights_state(_pathligths, false)
+    log.debug("[turn_nightpath_lights_on]: turning path lights on")
+    set_lights_state(settings.pathligths, true)
 }
 
-def _set_lights_state(targets, state)
+def turn_nightpath_lights_off()
+{
+    log.debug("[turn_nightpath_lights_off]: turning path lights off")
+    set_lights_state(settings.pathligths, false)
+}
+
+
+
+/*
+    Night Watch Utilities
+*/
+
+def send_push_alert(msg)
+{
+    log.debug("[send_push_alert]: sending push alert: ${msg}")
+    sendPush(msg)
+}
+
+
+
+/*
+    Global Utilities
+*/
+
+def set_lights_state(targets, state)
 {
     for (l in targets)
     {
@@ -181,4 +297,37 @@ def _set_lights_state(targets, state)
         else
             l.off()
     }
+}
+
+def is_light_sensor_in_dark()
+{
+    log.debug("[is_light_sensor_in_dark]: checking illuminance for ${lightsensordevice.displayName}: Current LUX: ${lightsensordevice.currentIlluminance}, Trigger LUX: ${settings.luminancetrigger}")
+
+    if (lightsensordevice.currentIlluminance > settings.luminancetrigger)
+    {
+        log.debug("[is_light_sensor_in_dark]: ${lightsensordevice.displayName} is above its illuminance trigger: not dark")
+        return false
+    }
+    else
+    {
+        log.debug("[is_light_sensor_in_dark]: ${lightsensordevice.displayName} is below its illuminance trigger: dark")
+        return true
+    }
+}
+
+def are_motion_sensors_active()
+{
+    for (device in settings.motionsensordevices)
+    {
+        if (device.currentValue("motion") == "active")
+        {
+            log.debug("[are_motion_sensors_active]: motion sensor ${device.displayName} is active -> active")
+
+            return true;
+        }
+    }
+
+    log.debug("[are_motion_sensors_active]: found 0 active motion sensor ->inactive")
+
+    return false;
 }
